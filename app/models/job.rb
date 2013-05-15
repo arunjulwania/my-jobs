0bc63a4e4e727601af2b65644b1ce9cd4913dc83
cis <cis@CISM455.(none)>
2013-05-15 15:24:45 +0530

    update

--- app/models/job.rb
+++ app/models/job.rb
@@ -1,7 +1,7 @@
 class Job < ActiveRecord::Base
   attr_accessible :company_name, :contact_number, :email, :experince, :job_title, :no_of_post, :qulification, :summary
     belongs_to :user 
-    searchable do
-     text  :company_name, :job_title ,:qulification
-    end
+    # searchable do
+    #  text  :company_name, :job_title ,:qulification
+    # end
 end
